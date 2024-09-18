8f3c72912460b94319cce792b057a8cba97560e8
y-h-haru04 <y.h.haru04@gmail.com>
2024-09-18 23:55:32 +0900

    fix: 演算子の追加等各種修正

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,15 @@ enum Vars {
     Function(Function),
 }
 
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum Value {
+    Num(i32),
+    Bool(bool),
+    List(List),
+    Str(String),
+    Nil,
+}
+
 struct Vm<'a> {
     input: &'a str,
     vars: Vec<HashMap<String, Vars>>,
@@ -25,16 +34,6 @@ impl<'a> Vm<'a> {
             vars: vec![HashMap::new()],
         }
     }
-}
-
-fn skip_whitespace(s: &str) -> &str {
-    let mut input = s;
-    while let Some(_ch @ (' ' | '\n')) = input.chars().next() {
-        let mut chars = input.chars();
-        chars.next();
-        input = chars.as_str();
-    }
-    input
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -60,27 +59,11 @@ impl fmt::Display for List {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Clone)]
-enum Value {
-    Num(i32),
-    Bool(bool),
-    List(List),
-    Nil,
-}
-
-impl Value {
-    fn as_num(&self) -> i32 {
-        match self {
-            Value::Num(i) => *i,
-            _ => panic!("Parse Error. Value: {self:?}"),
-        }
-    }
-}
-
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Value::Num(i) => write!(f, "{}", i),
+            Value::Str(s) => write!(f, "{}", s),
             Value::List(lst) => write!(f, "{}", lst.to_string()),
             _ => write!(f, ""),
         }
@@ -90,13 +73,12 @@ impl fmt::Display for Value {
 macro_rules! impl_op {
     {$name:ident, $op:tt} => {
         fn $name(vm: &mut Vm, input: &str) -> Value {
-            let (arg1, rest) = extract_next_chunk_with_rest(input);
-            let (arg2, _rest) = extract_next_chunk_with_rest(rest);
-            let v1 = eval(vm, arg1);
-            let v2 = eval(vm, arg2);
+            let tokens = extract_n_chunks(2, input);
+            let v1 = eval(vm, tokens.get(0).unwrap());
+            let v2 = eval(vm, tokens.get(1).unwrap());
             match (v1, v2) {
                 (Value::Num(i), Value::Num(j)) => Value::Num(i $op j),
-                _ => panic!("Parse error!"),
+                _ => panic!("Operator expects number."),
             }
         }
     }
@@ -119,20 +101,24 @@ fn cons(value: Value, list: List) -> List {
 
 fn eval_block(vm: &mut Vm, block: &str) -> Value {
     let inner = extract_block_inner(block);
-    let operator = extract_value(inner);
+    let operator = extract_token(inner);
     let op_len = operator.len();
     let rest = (&inner[op_len..]).trim();
 
     match operator {
         "cons" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, _) = extract_next_chunk_with_rest(rest);
+            let chunks = extract_n_chunks(2, rest);
+            let arg1 = chunks.get(0).unwrap();
+            let arg2 = chunks.get(1).unwrap();
+            if arg1.is_empty() {
+                return Value::List(List::Nil);
+            }
             let v1 = eval(vm, arg1);
             if arg2.len() > 0 {
                 let v2 = eval(vm, arg2);
                 match v2 {
                     Value::List(lst) => Value::List(cons(v1, lst)),
-                    _ => panic!("{v2:?} is not a list value"),
+                    _ => panic!("{v2:?} is not a list."),
                 }
             } else {
                 Value::List(cons(v1, List::Nil))
@@ -144,7 +130,7 @@ fn eval_block(vm: &mut Vm, block: &str) -> Value {
             match lst {
                 Value::List(List::Cons(lst)) => lst.value,
                 Value::List(List::Nil) => panic!("{arg:?} is a empty list."),
-                _ => panic!("{arg:?} is not a list value"),
+                _ => panic!("{arg:?} is not a list"),
             }
         }
         "cdr" => {
@@ -153,7 +139,15 @@ fn eval_block(vm: &mut Vm, block: &str) -> Value {
             match lst {
                 Value::List(List::Cons(lst)) => Value::List(lst.next),
                 Value::List(List::Nil) => panic!("{arg:?} is a empty list."),
-                _ => panic!("{arg:?} is not a list value"),
+                _ => panic!("{arg:?} is not a list."),
+            }
+        }
+        // 式展開を行わず文字列表現を返す
+        "#" => {
+            if is_block(rest) {
+                Value::Str(extract_block_inner(rest).into())
+            } else {
+                panic!("# expects block. found: {rest:?}")
             }
         }
         "-" => sub(vm, rest),
@@ -161,66 +155,61 @@ fn eval_block(vm: &mut Vm, block: &str) -> Value {
         "*" => mul(vm, rest),
         "/" => div(vm, rest),
         "<" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, _rest) = extract_next_chunk_with_rest(rest);
-            let v1 = eval(vm, arg1);
-            let v2 = eval(vm, arg2);
+            let tokens = extract_n_chunks(2, rest);
+            let v1 = eval(vm, tokens.get(0).unwrap());
+            let v2 = eval(vm, tokens.get(1).unwrap());
             match (v1, v2) {
                 (Value::Num(i), Value::Num(j)) => Value::Bool(i < j),
-                _ => panic!("Parse error!"),
+                _ => panic!("< expects number."),
             }
         }
-        "=" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, _rest) = extract_next_chunk_with_rest(rest);
-            let v1 = eval(vm, arg1);
-            let v2 = eval(vm, arg2);
+        "==" => {
+            let tokens = extract_n_chunks(2, rest);
+            let v1 = eval(vm, tokens.get(0).unwrap());
+            let v2 = eval(vm, tokens.get(1).unwrap());
             Value::Bool(v1 == v2)
         }
+        "!=" => {
+            let tokens = extract_n_chunks(2, rest);
+            let v1 = eval(vm, tokens.get(0).unwrap());
+            let v2 = eval(vm, tokens.get(1).unwrap());
+            Value::Bool(v1 != v2)
+        }
         "if" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, rest) = extract_next_chunk_with_rest(rest);
-            let (arg3, _rest) = extract_next_chunk_with_rest(rest);
-            let bool_result = eval_block(vm, arg1);
+            let chunks = extract_n_chunks(3, rest);
+            let bool_result = eval_block(vm, chunks.get(0).unwrap());
             match bool_result {
                 Value::Bool(bool) => {
                     if bool {
-                        eval(vm, arg2)
+                        eval(vm, chunks.get(1).unwrap())
                     } else {
-                        eval(vm, arg3)
+                        eval(vm, chunks.get(2).unwrap())
                     }
                 }
-                _ => panic!("Not a bool value"),
+                _ => panic!("{bool_result:?} is not a boolean value."),
             }
         }
         "let" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, _) = extract_next_chunk_with_rest(rest);
-            let var = eval(vm, arg2);
-            vm.vars
-                .iter_mut()
-                .rev()
-                .nth(0)
-                .map(|mp| mp.insert(arg1.into(), Vars::Var(var)));
+            let chunks = extract_n_chunks(2, rest);
+            let value = eval(vm, chunks.get(1).unwrap());
+            let key = *chunks.get(0).unwrap();
+            assign(vm, key, Vars::Var(value));
             Value::Nil
         }
         "def" => {
-            let (arg1, rest) = extract_next_chunk_with_rest(rest);
-            let (arg2, rest) = extract_next_chunk_with_rest(rest);
-            let (arg3, _rest) = extract_next_chunk_with_rest(rest);
+            let chunks = extract_n_chunks(3, rest);
+            let key = *chunks.get(0).unwrap();
+            let args = *chunks.get(1).unwrap();
+            let body = *chunks.get(2).unwrap();
             let function = Function {
-                args_block: arg2.into(),
-                body_block: arg3.into(),
+                args_block: args.into(),
+                body_block: body.into(),
             };
-            vm.vars
-                .iter_mut()
-                .rev()
-                .nth(0)
-                .map(|mp| mp.insert(arg1.into(), Vars::Function(function)));
+            assign(vm, key, Vars::Function(function));
             Value::Nil
         }
         "print" => {
-            let (arg, _rest) = extract_next_chunk_with_rest(rest);
+            let (arg, _) = extract_next_chunk_with_rest(rest);
             let value = eval(vm, arg);
             print(&value);
             value
@@ -235,20 +224,27 @@ fn eval_block(vm: &mut Vm, block: &str) -> Value {
                         args_block,
                         body_block,
                     }) => {
-                        let mut _rest = rest;
-                        // 関数の評価時に新スコープ分の変数用HashMapを追加
-                        let mut scoped_vars_container = HashMap::new();
                         // 関数の各変数に値をバインドして変数として宣言
-                        let variables = extract_block_inner(&args_block).split_whitespace();
-                        for variable in variables {
-                            let (arg, rest) = extract_next_chunk_with_rest(_rest);
-                            let var = Vars::Var(eval(vm, arg));
-                            scoped_vars_container.insert(variable.into(), var);
-                            _rest = rest;
+                        let variables = extract_block_inner(&args_block)
+                            .split_whitespace()
+                            .into_iter()
+                            .collect::<Vec<&str>>();
+                        let args = extract_n_chunks(variables.len(), rest);
+
+                        // 関数の評価時に新スコープ分の変数用HashMapを追加
+                        let scoped_vars_container = variables
+                            .iter()
+                            .map(|&key| key.into())
+                            .zip(args.iter().map(|arg| Vars::Var(eval(vm, arg))))
+                            .collect::<HashMap<String, Vars>>();
+                        let is_empty = scoped_vars_container.is_empty();
+                        if !is_empty {
+                            vm.vars.push(scoped_vars_container);
                         }
-                        vm.vars.push(scoped_vars_container);
                         let result = eval_block(vm, &body_block);
-                        vm.vars.pop();
+                        if !is_empty {
+                            vm.vars.pop();
+                        }
                         return result;
                     }
                 }
@@ -256,6 +252,35 @@ fn eval_block(vm: &mut Vm, block: &str) -> Value {
             Value::Nil
         }
     }
+}
+
+fn assign(vm: &mut Vm, key: &str, var: Vars) {
+    vm.vars
+        .iter_mut()
+        .rev()
+        .nth(0)
+        .and_then(|mp| mp.insert(key.into(), var));
+}
+
+fn skip_whitespace(s: &str) -> &str {
+    let mut input = s;
+    while let Some(_ch @ (' ' | '\n')) = input.chars().next() {
+        let mut chars = input.chars();
+        chars.next();
+        input = chars.as_str();
+    }
+    input
+}
+
+fn extract_n_chunks(n: usize, chunk: &str) -> Vec<&str> {
+    let mut buff = Vec::new();
+    let mut rest = chunk;
+    for _ in 0..n {
+        let (token, _rest) = extract_next_chunk_with_rest(rest);
+        buff.push(token);
+        rest = _rest;
+    }
+    buff
 }
 
 fn extract_next_chunk_with_rest(chunk: &str) -> (&str, &str) {
@@ -276,11 +301,11 @@ fn eval(vm: &mut Vm, value: &str) -> Value {
             if let Some(var) = vars.get(symbol) {
                 match var {
                     Vars::Var(v) => return v.clone(),
-                    _ => panic!("Functino can't be called"),
+                    _ => panic!("variable {var:?} is a function."),
                 }
             }
         }
-        panic!("Var: {value:?} isn't yet declared");
+        panic!("variable {value:?} hasn't yet declared.");
     }
 }
 
@@ -288,7 +313,7 @@ fn is_block(input: &str) -> bool {
     input.trim().starts_with('(')
 }
 
-// "(" と ")"のブロックを取得する
+// 対応する"(" と ")"の間の文字列を抜き出す
 fn extract_block<'a>(s: &'a str) -> &'a str {
     let mut i = 0;
     let mut paren_cnt = 0;
@@ -309,26 +334,23 @@ fn extract_block<'a>(s: &'a str) -> &'a str {
 }
 
 // 空白・改行までの文字列を取り出す
-fn extract_value<'a>(s: &'a str) -> &'a str {
+fn extract_token(input: &str) -> &str {
     let mut i = 0;
-    let _input = (&s[..]).trim();
-    let mut rest = _input.chars();
+    let mut rest = input.chars();
     while let Some(_char) = rest.next() {
         if _char == ' ' || _char == '\n' {
             break;
         }
         i += 1;
     }
-    &_input[0..i]
+    &input[0..i]
 }
 
-fn extract_next_chunk<'a>(s: &'a str) -> &'a str {
-    let _input = (&s[..]).trim();
-
-    if let Some('(') = _input.chars().next() {
-        extract_block(_input)
+fn extract_next_chunk(input: &str) -> &str {
+    if let Some('(') = input.chars().next() {
+        extract_block(input)
     } else {
-        extract_value(_input)
+        extract_token(input)
     }
 }
 
@@ -339,20 +361,14 @@ fn extract_block_inner<'a>(block: &'a str) -> &'a str {
 }
 
 fn parse(vm: &mut Vm) {
-    // println!("####");
-    // println!("{:?}", vm.input);
     let input = skip_whitespace(vm.input);
     // 基底部
-    if input.len() <= 0 {
+    if input.is_empty() {
         return;
     }
     let block = extract_block(input);
     let _result = eval_block(vm, block);
-    let ln = block.len();
-    vm.input = &vm.input.get((ln + 1)..).unwrap_or("").trim();
-    // println!("{block:?}");
-    // println!("result: {_result:?}");
-    // println!("{:?}", vm.vars);
+    vm.input = &vm.input.get((block.len() + 1)..).unwrap_or("").trim();
     parse(vm);
 }
 
@@ -362,36 +378,52 @@ fn main() {
     (if (< x 1)
         acc
         (product (- x 1) (* acc x))))
+
 (print (product 10 1))
-";
-    let input = "
-(let lst (cons 40 (cons 30 (cons 20 (cons 10 (cons 1))))))
-(let fst (car lst))
-(let rest (cdr lst))
-(print (car (cdr (cdr lst))))
+
+(def _initialize (x acc)
+    (if (== x 0)
+        acc
+        (_initialize (- x 1) (cons 0 acc))))
+
+(def initialize (x)
+    (_initialize x (cons)))
+
+(def insert (list index value)
+    (if (== 0 index)
+        (cons value list)
+        (cons (car list) (insert (cdr list) (- index 1) value))))
+
+(let list (initialize 100))
+
+(def n_times (n cb dummy)
+    (if (== n 0)
+        (cb)
+        (n_times (- n 1) cb (cb))))
+
+(def modulo (a b)
+    (- a (* b (/ a b))))
+
+(def fizzbuzz (n)
+    (if (== n 0)
+        n
+        (if (== (modulo n 3) 0)
+            (if (== (modulo n 5) 0)
+                (print (# (FizzBuzz\n)))
+                (print (# (Fizz\n))))
+            (if (== (modulo n 5) 0)
+                (print (# (Buzz\n)))
+                (print n)))))
+
+(def n_times_fizzbuzz (n dummy)
+    (if (== n 0)
+        (print (# (\nend\n)))
+        (n_times_fizzbuzz (- n 1) (fizzbuzz n))))
+
+(print (# (hoshi yuta desu\n)))
+(fizzbuzz 15)
+(n_times_fizzbuzz 20 0)
 ";
     let mut vm = Vm::new(input);
     parse(&mut vm);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse01() {
-        let expr = "(+ 10 20)";
-        assert_eq!(true, true);
-    }
-
-    #[test]
-    fn test_recursive_function() {
-        let input = "
-(def product (x acc)
-    (if (< x 1)
-        acc
-        (product (- x 1) (* acc x))))
-(product 10 1)
-";
-    }
 }
